e69421d0dccba494d727a495db149756331ab958
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-03-01 07:39:08 +0100

    enabled filtering event list by pipe-separated search terms

--- Gui/Models/Extensions.cs
+++ Gui/Models/Extensions.cs
@@ -17,6 +17,12 @@ internal static class StringExtensions
         // Trim leading/trailing whitespace
         return result.Trim();
     }
+
+    /// <summary>Indicates whether <paramref name="text"/> contains any of the supplied
+    /// <paramref name="terms"/> using <paramref name="stringComparison"/> to compare.</summary>
+    internal static bool ContainsAny(this string text, IEnumerable<string> terms,
+        StringComparison stringComparison = StringComparison.InvariantCultureIgnoreCase)
+        => terms.Any(t => text.Contains(t, stringComparison));
 }
 
 internal static class EnumerableExtensions
