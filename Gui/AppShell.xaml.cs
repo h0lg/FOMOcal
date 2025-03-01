34693768b61ee2a52bbb4cd633b0292bbce04980
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-03-01 07:50:41 +0100

    enabled navigating links to event and ticket URLs
    using a WebView if in Shell, otherwise Launcher

--- Gui/AppShell.xaml.cs
+++ Gui/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace FomoCal.Gui
+﻿using FomoCal.Gui.ViewModels;
+
+namespace FomoCal.Gui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(WebViewPage), typeof(WebViewPage));
         }
     }
 }
