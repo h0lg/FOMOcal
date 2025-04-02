33b4a00a9f429cedac2516481b47efb319e6c754
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-04-02 22:37:25 +0200

    reusing the same lazy-initialized CultureInfo instance
    instead of re-creating it

--- Gui/Models/DateScrapeJob.cs
+++ Gui/Models/DateScrapeJob.cs
@@ -4,21 +4,34 @@ namespace FomoCal;
 
 public class DateScrapeJob : ScrapeJob
 {
+    private string culture = "en";
+    private CultureInfo? cultureInfo;
+
     public string Format { get; set; } = "dd MMM yyyy";
-    public string Culture { get; set; } = "en";
+
+    public string Culture
+    {
+        get { return culture; }
+        set
+        {
+            culture = value;
+            cultureInfo = null;
+        }
+    }
+
+    private CultureInfo CultureInfo => cultureInfo ??= new(Culture);
 
     public DateTime? GetDate(AngleSharp.Dom.IElement element)
     {
         var rawValue = base.GetValue(element);
         if (string.IsNullOrWhiteSpace(rawValue)) return null;
-        CultureInfo culture = new(Culture);
         const DateTimeStyles style = DateTimeStyles.None;
 
-        if (DateTime.TryParseExact(rawValue, Format, culture, style, out DateTime parsedDate)) return parsedDate;
+        if (DateTime.TryParseExact(rawValue, Format, CultureInfo, style, out DateTime parsedDate)) return parsedDate;
 
         if (!Format.Contains('y') // retry parsing date as next year's for format without year info
             && DateTime.TryParseExact(rawValue + " " + (DateTime.Today.Year + 1), Format + " yyyy",
-                culture, style, out DateTime nextYearsDate)) return nextYearsDate;
+                CultureInfo, style, out DateTime nextYearsDate)) return nextYearsDate;
 
         throw new Error($"Failed to parse date '{rawValue}' using format '{Format}' in culture '{Culture}'.");
     }
