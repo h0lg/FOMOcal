76eac0efe17eb0ebdc37eee0b5538dbc0e80a7c0
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-08-23 18:31:54 +0200

    publishing single file and creating versioned zip
    to make finding executable easier
    and publishing quicker

--- Gui/Platforms/Windows/publish.cmd
+++ Gui/Platforms/Windows/publish.cmd
@@ -1,3 +1,3 @@
 cd ..\..
-dotnet publish -f net9.0-windows10.0.19041.0 -c Release -p:RuntimeIdentifierOverride=win-x64
+dotnet publish -f net9.0-windows10.0.19041.0 -c Release --self-contained false -p:RuntimeIdentifierOverride=win-x64 -p:PublishSingleFile=true -p:PublishReadyToRun=true
 PAUSE
