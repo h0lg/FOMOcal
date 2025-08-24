df8f4f56b0384b2c587a9e7547d62117dfbd8ef9
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-08-24 03:02:46 +0200

    enhanced HTML export by including simple-datatables via CDN

--- Gui/Models/Export.cs
+++ Gui/Models/Export.cs
@@ -140,15 +140,20 @@ internal static partial class Export
         var doc = await context.OpenNewAsync();
 
         // styling
+        var link = (IHtmlLinkElement)doc.CreateElement("link");
+        link.Href = "https://unpkg.com/simple-datatables@10.0.0/dist/style.css";
+        link.Type = "text/css";
+        link.Relation = "stylesheet";
+        doc.Head!.AppendChild(link);
         var style = doc.CreateElement("style");
 
         style.TextContent =
-@"table { border-collapse: collapse; font-family: sans-serif; }
-th, td { border: 1px solid #ccc; padding: 4px 8px; }
+@"table { font-family: sans-serif; }
 img { max-height: 100px; }";
 
         doc.Head!.AppendChild(style);
         var table = doc.CreateElement("table");
+        table.Id = "events";
         var thead = doc.CreateElement("thead");
         var headerRow = doc.CreateElement("tr");
 
@@ -213,6 +218,14 @@ img { max-height: 100px; }";
 
         table.AppendChild(tbody);
         doc.Body!.AppendChild(table);
+
+        var tablesInclude = (IHtmlScriptElement)doc.CreateElement("script");
+        tablesInclude.Source = "https://unpkg.com/simple-datatables@10.0.0/dist/umd/simple-datatables.js";
+        doc.Body!.AppendChild(tablesInclude);
+        var initScript = (IHtmlScriptElement)doc.CreateElement("script");
+        initScript.Text = $"new simpleDatatables.DataTable('#{table.Id}')";
+        doc.Body!.AppendChild(initScript);
+
         await ExportFile(fileTypeLabel: "HTML", contents: doc.ToHtml(), extension: "html", contentType: MediaTypeNames.Text.Html);
     }
 
