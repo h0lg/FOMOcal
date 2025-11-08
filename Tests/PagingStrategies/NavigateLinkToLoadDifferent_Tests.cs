13bc5fb227261a2e123c66db1b0a833beb50cca7
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-11-08 16:09:09 +0100

    added test verifying that scraper tries paging at least once if current page only contains past events

--- Tests/PagingStrategies/NavigateLinkToLoadDifferent_Tests.cs
+++ Tests/PagingStrategies/NavigateLinkToLoadDifferent_Tests.cs
@@ -53,4 +53,26 @@ public sealed partial class NavigateLinkToLoadDifferent_Tests : PagingStrategyTe
 
         Assert.HasCount(10, events);
     }
+
+    /* some venues show only events in the current month on the first page
+     * - which may all already be in the past when visited towards the end of the month */
+    [TestMethod]
+    public async Task TriesPagingAtLeastOnceIfCurrentPageContainsOnlyPastEvents()
+    {
+        browser.AddEvents(venue, 10, error: EventPageError.Past);
+        browser.AddNextPageLink("#page-1");
+
+        browser.AddEvents(venue, 10, start: 11, page: 1);
+
+        (var events, var errors) = await scraper.ScrapeVenueAsync(venue);
+        AssertEmpty(errors);
+
+        AssertLogLines(
+            "found 10 events, 10 in the past - i.e. 10 irrelevant",
+            "next page link goes to #page-1",
+            "found 10 events",
+            "found 10 relevant events in total");
+
+        Assert.HasCount(10, events);
+    }
 }
