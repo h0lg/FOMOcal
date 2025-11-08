b74cbfa149c824aa85b5a8182497259aeaba7f03
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-11-08 15:38:35 +0100

    testing that paging doesn't stop for events excluded by filter

--- Tests/PagingStrategies/NavigateLinkToLoadDifferent_Tests.cs
+++ Tests/PagingStrategies/NavigateLinkToLoadDifferent_Tests.cs
@@ -26,4 +26,31 @@ public sealed partial class NavigateLinkToLoadDifferent_Tests : PagingStrategyTe
 
         Assert.HasCount(20, events);
     }
+
+    /* Sommerloch - during festival season, bands play festivals instead of venues
+     * leaving only pages after pages of repeating parties in the programs of some venues  */
+    [TestMethod]
+    public async Task AllExcludedByFilterWontStopPaging()
+    {
+        browser.AddEvents(venue, 10, category: "boring party");
+        browser.AddNextPageLink("#page-1");
+
+        browser.AddEvents(venue, 10, category: "boring party", start: 11, page: 1);
+        browser.AddNextPageLink("#page-2", page: 1);
+
+        browser.AddEvents(venue, 10, start: 21, page: 2);
+
+        (var events, var errors) = await scraper.ScrapeVenueAsync(venue);
+        AssertEmpty(errors);
+
+        AssertLogLines(
+            "found 10 events, 0 matched by concert",
+            "next page link goes to #page-1",
+            "found 10 events, 0 matched by concert",
+            "next page link goes to #page-2",
+            "found 10 events",
+            "found 30 relevant events in total");
+
+        Assert.HasCount(10, events);
+    }
 }
