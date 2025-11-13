87e6496ac9807af580c4684c6f9959bcd528f519
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-11-13 01:32:20 +0100

    added test verifying that paging stops when hitting past events only

--- Tests/PagingStrategies/ScrollDownToLoadMore_Tests.cs
+++ Tests/PagingStrategies/ScrollDownToLoadMore_Tests.cs
@@ -24,4 +24,23 @@ public sealed partial class ScrollDownToLoadMore_Tests : PagingStrategyTests
 
         Assert.HasCount(20, events);
     }
+
+    [TestMethod]
+    public async Task PagingStopsWhenHittingPastEventsOnly()
+    {
+        browser.AddEvents(venue, 10, start: 11);
+        browser.AddEvents(venue, 10, page: 1);
+        browser.AddEvents(venue, 10, start: -10, page: 2);
+        browser.AddEvents(venue, 10, start: -20, page: 3);
+
+        (var events, var errors) = await scraper.ScrapeVenueAsync(venue);
+        Assert.HasCount(20, events);
+        AssertEmpty(errors);
+
+        AssertLogLines(
+            "selected 10 events",
+            "selected 20 events -10 already scraped",
+            "selected 30 events -10 in the past -20 already scraped",
+            "scraped 20 events in total");
+    }
 }
