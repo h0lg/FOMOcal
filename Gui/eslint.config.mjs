6607a5bf9e81a8d066f395665d4fbe9bf9df87e2
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-11-27 01:54:33 +0100

    updated eslint

--- Gui/eslint.config.mjs
+++ Gui/eslint.config.mjs
@@ -1,14 +1,13 @@
+import js from "@eslint/js";
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import { defineConfig } from "eslint/config";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
     { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
     {
         rules: {
             semi: ["error", "always"]
         }
     }
-];
+]);
