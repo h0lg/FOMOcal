ee66dc632f3c129f2233bef0c24a5b7c48cd1890
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-12-05 03:52:03 +0100

    upgraded to .net 10

--- Gui/Platforms/Windows/publish.cmd
+++ Gui/Platforms/Windows/publish.cmd
@@ -1,3 +1,3 @@
 cd ..\..
-dotnet publish -f net9.0-windows10.0.19041.0 -c Release --self-contained false -p:RuntimeIdentifierOverride=win-x64 -p:PublishSingleFile=true -p:PublishReadyToRun=true
+dotnet publish -f net10.0-windows10.0.19041.0 -c Release --no-self-contained -p:RuntimeIdentifierOverride=win-x64 -p:PublishSingleFile=true -p:PublishReadyToRun=true
 PAUSE
