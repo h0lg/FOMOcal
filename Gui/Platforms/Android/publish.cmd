daeec251677800250ef052bd3139a0a759792611
Holger Schmidt <leroyz.mailaccount@gmx.net>
2026-01-04 22:34:02 +0100

    updated android publish command

--- Gui/Platforms/Android/publish.cmd
+++ Gui/Platforms/Android/publish.cmd
@@ -1,4 +1,4 @@
 cd ..\..
 REM Use AndroidPackageFormat=aab for Play Store publishing, or apk for sideloading/testing
-dotnet publish -f net9.0-android -c Release -p:AndroidPackageFormat=apk
+dotnet publish -f net10.0-android -c Release -p:AndroidPackageFormat=apk
 PAUSE
