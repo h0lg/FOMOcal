7a5d2628113d670591174c99bb823b00ec579d28
Holger Schmidt <leroyz.mailaccount@gmx.net>
2026-05-22 23:21:38 +0200

    Windows OS: fixed window button icon colors, making them visible

--- Gui/App.xaml.cs
+++ Gui/App.xaml.cs
@@ -35,8 +35,49 @@
                 rootPage = new NavigationPage(mainPage);
             }
 
-            return new(rootPage) { Title = "FOMOcal" };
+            Window window = new(rootPage) { Title = "FOMOcal" };
+
+#if WINDOWS
+            window.Created += async (_, _) =>
+            {
+                await EnsureButtonsAreVisible(window);
+                Current!.RequestedThemeChanged += async (o, e) => await EnsureButtonsAreVisible(window);
+            };
+#endif
+
+            return window;
         }
+
+#if WINDOWS
+        // from https://github.com/dotnet/maui/blob/main/src/Controls/src/Core/Application/Application.Windows.cs
+        public static readonly Windows.UI.Color LightHoverBackground = Microsoft.UI.ColorHelper.FromArgb(24, 0, 0, 0);
+        private static readonly Windows.UI.Color DarkHoverBackground = Microsoft.UI.ColorHelper.FromArgb(24, byte.MaxValue, byte.MaxValue, byte.MaxValue);
+
+        /// <summary>Applies styles to the Window buttons after <see cref="Application.RequestedThemeChanged"/>
+        /// to make them visible while <see cref="AppWindowTitleBar.BackgroundColor"/> is controlled by the system theme.</summary>
+        private static async ValueTask EnsureButtonsAreVisible(Window window)
+        {
+            var nativeWindow = window.Handler?.PlatformView as MauiWinUIWindow;
+            if (nativeWindow?.AppWindow?.TitleBar is not Microsoft.UI.Windowing.AppWindowTitleBar titleBar) return;
+            await Task.Delay(50); // to run after the default OnRequestedThemeChangedPlatform handler
+            bool systemDark = Current?.PlatformAppTheme == AppTheme.Dark;
+            bool dark = Current?.RequestedTheme == AppTheme.Dark;
+
+            if (systemDark && !dark)
+            {
+                titleBar.ButtonHoverForegroundColor =
+                titleBar.ButtonForegroundColor = Microsoft.UI.Colors.White; // so icons are visible
+                titleBar.ButtonHoverBackgroundColor = DarkHoverBackground; // so they visually react to hover
+            }
+
+            if (!systemDark)
+            {
+                titleBar.ButtonHoverForegroundColor =
+                titleBar.ButtonForegroundColor = Microsoft.UI.Colors.Black; // so icons are visible
+                titleBar.ButtonHoverBackgroundColor = LightHoverBackground; // so they visually react to hover
+            }
+        }
+#endif
 
         internal static Page GetCurrentContentPage()
         {
