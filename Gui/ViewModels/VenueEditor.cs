e3e0bd70536afef6e776e8ca8effa8b3513bc817
Holger Schmidt <leroyz.mailaccount@gmx.net>
2026-07-03 23:51:22 +0200

    Revert "Venue Editor: simplified cancelation overriding OnDisappearing"
    so that navigating forward to the PickUrlPage doesn't cancel, making later saving impossible

--- Gui/ViewModels/VenueEditor.cs
+++ Gui/ViewModels/VenueEditor.cs
@@ -158,6 +158,14 @@ public partial class VenueEditor : ObservableObject
     }
 
     private void Delete() => SetActionTaken(Actions.Deleted);
+
+    [RelayCommand]
+    private async Task CancelFromNavBarAsync()
+    {
+        SignalCancelation();
+        await navigation.PopAsync(); // to unify with OnBackButtonPressed behavior; awaiter expects popped navigation stack
+    }
+
     private void SignalCancelation() => SetActionTaken(null);
 
     private void SetActionTaken(Actions? action)
@@ -174,6 +182,9 @@ public partial class VenueEditor : ObservableObject
 
         public Page(VenueEditor model)
         {
+            // unify NavBar Back button with OnBackButtonPressed behavior triggered via key or swipe gesture on Android
+            Shell.SetBackButtonBehavior(this, new BackButtonBehavior { Command = model.CancelFromNavBarCommand });
+
             this.model = model;
             BindingContext = model;
             Title = model.isDeletable ? "Edit " + model.originalVenueName : "Add a venue";
@@ -298,11 +309,10 @@ public partial class VenueEditor : ObservableObject
                 return (selector, picksDescendant);
             });
 
-        protected override void OnDisappearing()
+        protected override bool OnBackButtonPressed()
         {
-            base.OnDisappearing();
-            // ensure a cancelation result is returned if the user navigates back manually without saving or deleting
-            model.SignalCancelation();
+            model!.SignalCancelation();
+            return base.OnBackButtonPressed();
         }
     }
 }
