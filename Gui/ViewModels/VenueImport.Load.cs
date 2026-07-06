a21488a1ae5e7790027f8222f769ee0d9a84a2b6
Holger Schmidt <leroyz.mailaccount@gmx.net>
2026-07-07 00:58:15 +0200

    Venue Import: added confirmations for proceeding when importing an unexpected file extension or content type from the web

--- Gui/ViewModels/VenueImport.Load.cs
+++ Gui/ViewModels/VenueImport.Load.cs
@@ -1,4 +1,6 @@
-﻿namespace FomoCal.Gui.ViewModels;
+﻿using System.Net.Mime;
+
+namespace FomoCal.Gui.ViewModels;
 
 public partial class VenueImport
 {
@@ -15,17 +17,48 @@ public partial class VenueImport
 
         if (source == fromUrl)
         {
+            if (!App.HasInternet)
+            {
+                await App.CurrentPage.DisplayAlertAsync("Connect the internet first",
+                    "...and then we can download from there. Sounds good?", "Sure");
+
+                return null;
+            }
+
             var url = await App.CurrentPage.DisplayPromptAsync("Import venues from the web",
                 "Enter the URL to the JSON file to import",
                 placeholder: "https://some.page/venues.json");
 
             if (url == null) return null;
 
+            if (!url.EndsWith(".json"))
+            {
+                var proceed = await App.CurrentPage.DisplayAlertAsync("Unexpected file extension",
+                    "That link doesn't have the .json file extension. Try to download a file from it anyway?",
+                    accept: "Yes", cancel: "No");
+
+                if (!proceed) return null;
+            }
+
             try
             {
                 using HttpClient httpClient = new();
                 using var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
                 response.EnsureSuccessStatusCode();
+
+                const string json = MediaTypeNames.Application.Json;
+                string? mediaType = response.Content.Headers.ContentType?.MediaType;
+
+                if (mediaType != json)
+                {
+                    var proceed = await App.CurrentPage.DisplayAlertAsync("Unexpected content type",
+                        $"The content type {mediaType} of the downloaded file is not {json}."
+                            + " Try to load venues from it anyway?",
+                        accept: "Yes", cancel: "No");
+
+                    if (!proceed) return null;
+                }
+
                 await using var stream = await response.Content.ReadAsStreamAsync();
                 return await JsonFileStore.DeserializeFromAsync<HashSet<Venue>>(stream);
             }
